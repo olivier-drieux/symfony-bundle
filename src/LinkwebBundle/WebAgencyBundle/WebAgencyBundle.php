7db1e87798897633263bdc273213831863508883
Jordan blasenhauer <jordan.blasenhauer@linkweb.fr>
2025-03-17 13:54:05 +0100

    feat: Add bundle migration to main app without updating main app

--- src/LinkwebBundle/WebAgencyBundle/WebAgencyBundle.php
+++ src/LinkwebBundle/WebAgencyBundle/WebAgencyBundle.php
@@ -4,10 +4,24 @@ namespace App\LinkwebBundle\WebAgencyBundle;
 
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
 /**
  * @link https://symfony.com/doc/current/bundles/best_practices.html
  */
 class WebAgencyBundle extends AbstractBundle
 {
+    /**
+     * @param ContainerBuilder $container
+     *
+     * @throws \Exception
+     */
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
 
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/config/packages'));
+        $loader->load('doctrine_migrations.yaml');
+    }
 }
