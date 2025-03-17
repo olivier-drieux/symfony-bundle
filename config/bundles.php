ea15c097da8b70eb50f34aa6b6b82a204383920a
Olivier Drieux <olivier.drieux@linkweb.fr>
2025-03-17 08:48:33 +0100

    wip

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Linkweb\SharedBundle\SharedBundle::class => ['all' => true],
 ];
