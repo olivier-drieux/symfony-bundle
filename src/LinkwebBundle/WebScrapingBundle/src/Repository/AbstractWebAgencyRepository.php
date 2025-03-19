69710b4b0c4edb2d2a9c246596a52c12cee7eff5
Jordan blasenhauer <jordan.blasenhauer@linkweb.fr>
2025-03-19 10:04:46 +0100

    feat: Add abstractRepository

---
+++ src/LinkwebBundle/WebScrapingBundle/src/Repository/AbstractWebAgencyRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\LinkwebBundle\WebScrapingBundle\src\Repository;
+
+use App\Entity\WebAgency;
+use Doctrine\Persistence\ManagerRegistry;
+use Linkweb\Repository\AbstractRepository;
+
+/**
+ * @method WebAgency|null find($id, $lockMode = null, $lockVersion = null)
+ * @method WebAgency|null findOneBy(array $criteria, array $orderBy = null)
+ * @method WebAgency[]    findAll()
+ * @method WebAgency[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method WebAgency|null findOneById($id)
+ */
+abstract class AbstractWebAgencyRepository extends AbstractRepository
+{
+    /**
+     * WebAgencyRepository constructor.
+     *
+     * @param ManagerRegistry $registry
+     */
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, WebAgency::class);
+    }
+}
