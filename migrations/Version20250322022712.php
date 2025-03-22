7d89739f13eb9dbfc3cf40e5af464aef0a813c42
Olivier DRIEUX <olivierd47000@gmail.com>
2025-03-22 03:29:38 +0100

    fix: Prevent both app and bundle WebAgency from being detected. Only the app

--- migrations/Version20250322022712.php
+++ migrations/Version20250322022712.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250321224310 extends AbstractMigration
+final class Version20250322022712 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,9 +20,10 @@ final class Version20250321224310 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE project (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', name VARCHAR(255) NOT NULL, data JSON NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE project (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', web_agency_id INT NOT NULL, name VARCHAR(255) NOT NULL, data JSON NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_2FB3D0EE13461AFE (web_agency_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE project_mockup (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', project_id CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:uuid)\', domain_name VARCHAR(255) NOT NULL, theme VARCHAR(255) NOT NULL, is_mockup TINYINT(1) DEFAULT 1 NOT NULL, login VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, data JSON NOT NULL, image_zip_path VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_66323652166D1F9C (project_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE `user` (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', email VARCHAR(180) NOT NULL, username VARCHAR(255) NOT NULL, api_key VARCHAR(255) DEFAULT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE project ADD CONSTRAINT FK_2FB3D0EE13461AFE FOREIGN KEY (web_agency_id) REFERENCES web_agency (id)');
         $this->addSql('ALTER TABLE project_mockup ADD CONSTRAINT FK_66323652166D1F9C FOREIGN KEY (project_id) REFERENCES project (id)');
         $this->addSql('ALTER TABLE web_agency ADD extra_field VARCHAR(255) DEFAULT NULL, DROP version, CHANGE website website VARCHAR(255) NOT NULL');
     }
@@ -30,6 +31,7 @@ final class Version20250321224310 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE project DROP FOREIGN KEY FK_2FB3D0EE13461AFE');
         $this->addSql('ALTER TABLE project_mockup DROP FOREIGN KEY FK_66323652166D1F9C');
         $this->addSql('DROP TABLE project');
         $this->addSql('DROP TABLE project_mockup');
