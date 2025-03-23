1740a467c4cf4fc7175cb37f45ef1eaeb2914db2
Olivier Drieux <olivier.drieux@linkweb.fr>
2025-03-23 14:44:07 +0100

    feat: Working bundle routes

--- migrations/Version20250323131307.php
+++ migrations/Version20250323131307.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250322022712 extends AbstractMigration
+final class Version20250323131307 extends AbstractMigration
 {
     public function getDescription(): string
     {
